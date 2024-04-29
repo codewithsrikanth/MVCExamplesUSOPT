40e9b2b61dfa95dc7da599aac2220c0fcecc1e10
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-04-29 21:27:03 +0530

    Views in MVC

---
+++ ControllerInMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ControllerInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string empName = "Srikanth";
+            double salary = 25000;double inc = 2450;
+            Response.Write("Employee Name is: " + empName);
+            double totalSalary = GetTotal(salary, inc);
+            Response.Write("Total Salary: " + totalSalary);
+            return View();
+        }
+        [NonAction] 
+        public double GetTotal(double salary, double inc)
+        {
+            return salary+inc;
+        }
+        public ActionResult GetServerTime()
+        {
+            string str = "Server Time is: " + DateTime.Now.ToString();
+            return Content(str);
+        }
+    }
+}
---
+++ ViewsInMVC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace ViewsInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
