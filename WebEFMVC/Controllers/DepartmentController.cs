2d85c194f3f2f5b5042421cb5ae549d38401cd49
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-05-15 19:18:03 +0530

    EF Mulitple Table Relations DB First Approch

---
+++ WebEFMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebEFMVC.Models;
+
+namespace WebEFMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public ActionResult Index()
+        {
+            CompanyDBEntities dbContext = new CompanyDBEntities();
+            List<Department> departments =  dbContext.Departments.ToList();
+            return View(departments);
+        }
+    }
+}
---
+++ WebEFMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebEFMVC.Models;
+
+namespace WebEFMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(int departmentId)
+        {
+            CompanyDBEntities dbContext = new CompanyDBEntities();
+            List<EmpDetail> emps = dbContext.EmpDetails.Where(emp => emp.DepartmentId == departmentId).ToList();
+            return View(emps);
+        }
+        public ActionResult Details(int id)
+        {
+            CompanyDBEntities dbContext = new CompanyDBEntities();
+            EmpDetail empDetail = dbContext.EmpDetails.Where(emp=>emp.EmployeeId == id).FirstOrDefault();
+            return View(empDetail);
+        }
+    }
+}
