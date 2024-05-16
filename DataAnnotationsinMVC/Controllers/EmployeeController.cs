08deba5d9c9cfb58f570372f3846ff40fe86d571
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-05-16 19:50:11 +0530

    Data Annotations and Layout Views

---
+++ DataAnnotationsinMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using DataAnnotationsinMVC.Models;
+using System.Web.Mvc;
+
+namespace DataAnnotationsinMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.Msg = "Inserted Successfully";
+            }
+            else
+            {
+                ViewBag.Msg = "Validations Required";
+            }
+            
+            return View();
+        }
+    }
+}
---
+++ DataAnnotationsinMVC/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotationsinMVC.Models
+{
+    public class Employee
+    {
+        public Guid EmployeeId { get; set; }
+        [Required(ErrorMessage = "Firstname is mandatory to enter")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is mandatory to enter")]
+        [StringLength(30, MinimumLength = 5)]
+        public string LastName { get; set; }
+
+        [Range(25, 40, ErrorMessage = "Minimum age is 25 and max is 40")]
+        public int Age { get; set; }
+
+        [Range(typeof(DateTime), "01-01-1970", "01-01-2008", ErrorMessage = "DateOfBirth in between 01-01-1970 and 01-01-2008")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+
+        [MaxLength(25)]
+        [MinLength(5)]
+        public string Address { get; set; }
+
+        [RegularExpression("^([0]|\\+91)?\\d{10}")]
+        public string Mobile { get; set; }
+        public string PostalCode { get; set; }
+        [EmailAddress]
+        public string EmailId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public string Url { get; set; }
+    }
+}
