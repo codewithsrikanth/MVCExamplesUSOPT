5690d9cc0fc6d5e48421e9f7f232657cc7137aff
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-05-27 19:56:14 +0530

    Web API Token based security

---
+++ EmployeeWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeWebAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace EmployeeWebAPI.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [Authorize]
+        public IEnumerable<Employee> Get()
+        {
+            using(CompanyDBEntities entities = new CompanyDBEntities())
+            {
+                return entities.Employees.ToList();
+            };
+        }
+    }
+}
